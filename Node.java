61c4bf084d679c1306ce51cb7149aae6df03f982
reyhanhosavci <reyhangrlyn@gmail.com>
2023-04-13 14:24:36 +0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Assignment1_ReyhanHosavci;
+
+/**
+ *
+ * @author rgurleyen
+ */
+public class Node {
+    int data;
+    Node next;
+    Node up;
+    int location;
+    
+    public Node(int data, int location){
+    this.data=data;
+    this.location=location;
+    this.next=null;
+    this.up=up;
+}
+}
